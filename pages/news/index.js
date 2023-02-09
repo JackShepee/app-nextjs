28d98c7040f4ada8f207a5aa667a9a69c5cac443
JackShepee <salamakhaviktor@gmail.com>
2023-02-09 13:37:38 +0200

    1

---
+++ pages/news/index.js
@@ -0,0 +1,20 @@
+import { Fragment } from "react";
+import Link from 'next/link'; 
+
+const NewsPage = () => {
+  return (
+    <Fragment>
+      <h1>The News Page!</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is A great fraemwork
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
+};
+
+export default NewsPage;
